39ba180fa4d045f139b9e420cd52521d90adb84a
Byte_Hammer <bytehammer2@gmail.com>
2024-05-22 20:07:17 -0700

    Add modal interaction for creating new events

--- ScheDungeon Bot/Modules/CommandsModule.cs
+++ ScheDungeon Bot/Modules/CommandsModule.cs
@@ -1,4 +1,6 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
+using ScheDungeon.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,5 +29,42 @@ namespace ScheDungeon.Modules
         [SlashCommand("latency-ephemeral", "Debug message to test if my commands are being registered. Ephemeral version.")]
         public async Task PingEphemeralAsync()
             => await RespondAsync(text: $"DEBUG: Latency is {Context.Client.Latency}ms. Ephemeral", ephemeral: true);
+
+        [SlashCommand("create-event", "Create a new event that people can subscribe to for updates.")]
+        public async Task CreateNewEventAsync()
+        {
+            var cb = new ComponentBuilder()
+                .WithButton("Create Event", $"create_event_button:{Context.User.Id}");
+
+            await RespondAsync("Pressing the following button will create your event. Once it's created, people will be able to subscribe to your game for reminders, and you will be able to schedule sessions.", components: cb.Build());
+        }
+
+        [DoUserCheck]
+        [ComponentInteraction("create_event_button:*")]
+        public async Task CreateEventButtonPressedAsync()
+            => await Context.Interaction.RespondWithModalAsync<CreateEventModal>("create_event_modal");
+
+        public class CreateEventModal : IModal
+        {
+            public string Title => "Create an Event";
+
+            [InputLabel("Event Name")]
+            [ModalTextInput("event_name", placeholder: "Unnamed Event", maxLength:64)]
+            public string EventName { get; set; }
+
+            [InputLabel("Event Description")]
+            [ModalTextInput("event_description", TextInputStyle.Paragraph, placeholder: "Please enter a short description for your event.", maxLength: 500)]
+            public string EventDescription { get; set; }
+        }
+
+        [ModalInteraction("create_event_modal")]
+        public async Task CreateEventModalResponseAsync(CreateEventModal modal)
+        {
+            string response = $"I received your response to the 'Create an Event' dialog box. I don't store any info yet, but here's the data I received: {Environment.NewLine}" +
+                $"Event Name: {modal.EventName}{Environment.NewLine}" +
+                $"Event Description: {modal.EventDescription}";
+
+            await RespondAsync(response);
+        }
     }
 }
